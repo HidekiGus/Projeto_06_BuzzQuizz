b50633f3b14bff836d9ee281bd0b2dbf4ade12c8
Jéssica <jessicavitorsantos@outlook.com>
2022-04-21 19:25:10 -0300

    Página de Quizz

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
-// PEGAR QUIZZ DA API
+// PEGAR QUIZZES DA API (não tá funcionando :P)
+let quizzInfo;
+let disporQuizz = document.querySelector(".paginaQuizz");
 
-//https://www.10wallpaper.com/wallpaper/1366x768/1404/white_blossom_flowers-Plants_HD_Wallpaper_1366x768.jpg
+function carregarQuizzes () {
+     let promise = axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/24')
+
+    promise.then(quizzesServ);
+}
+
+carregarQuizzes()
+
+function quizzesServ (resposta) {
+  console.log(resposta.data) 
+
+    
+    
+
+}
+
