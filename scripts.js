15f700de15197b0bdd3e434f59a1993960d4832b
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-04-23 20:02:15 -0300

    criacao quizz:perguntas

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ let disporQuizz = document.querySelector(".paginaQuizz");
 let api = "https://mock-api.driven.com.br/api/v6/buzzquizz/";
 let lista_quizzes;
 
+
+
 function carregarQuizzes(id) {
   let promise = axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/' + id)
 
@@ -145,6 +147,133 @@ function voltar_home() {
 
 // CRIAÇÃO   DO    QUIZZ     --------------------------------
 
+
+
+// VERIFICAÇÕES DOS INPUTS     --------------------------------------
+
+// FUNÇÃO PARA VERIFICAR O NÚMERO DE CARACTERES 
+// A função pega a tag input , o numero minimo de caracteres e o maximo de caracteres
+function numCaracter( input, minCaracter,  maxCaracter )  {
+  let texto = input.value;
+  input.classList.toggle("invalid");
+  if(maxCaracter!==false){
+    if (texto.length >= minCaracter && texto.length <= maxCaracter) {
+      input.classList.remove("invalid")
+    }else {
+      input.classList.add("invalid")
+  }}else{
+    if (texto.length >= minCaracter) {
+      input.classList.remove("invalid")
+    }else {
+      input.classList.add("invalid")
+  }
+  }
+}
+
+// FUNÇÃO PARA VERIFICAR SE A COR É HEXADECIMAL
+function hexColor(input)  {
+
+  // Forma padrão de uma cor Hexadecimal
+  let formaHex = /^#[A-Fa-f0-9]{6}$/;
+  // ^ representa o início da string.
+  // # representa o código de cor hexadecimal deve começar com um símbolo '#'.
+  // [A-Fa-f0-9]{6} representa a letra de af, AF ou dígito de 0-9 com um comprimento de 6.
+  // $ representa o final da string.
+
+  let texto = input.value;
+
+  //  test() executa uma busca por uma correspondência entre uma expressão formaHex e a string texto
+  input.classList.toggle("invalid")
+  if(formaHex.test(texto)){
+    input.classList.remove("invalid")
+  }else {
+    input.classList.add("invalid")
+  }
+}
+
+// FUNÇÃO PARA VERIFICAR URL
+function verifURL(input)  {
+  // Forma padrão de uma url
+  let url = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  // Formato final de uma URL de Imagem deve seguir padrão abaixo
+  let verImg= /\.(jpg|jpeg|png|webp|avif|gif|svg)$/;
+  input.classList.toggle("invalid");
+  let texto = input.value;
+
+  // Na PÁGINA_DOIS a condição abaixo será necessária para invalidar o input que vazio da resposta incorreta 1 , visto que precisa de pelo menos uma resposta incorreta
+  if(input.classList[1]!==undefined){
+    if(input.classList[1]==="um"){
+      if(texto.length===0){
+        input.classList.add("invalid")
+      }
+    }
+  }
+  
+  if (texto.length!==0) {
+    if(url.test(texto) && verImg.test(texto)){
+      input.classList.remove("invalid")
+    }else {
+      input.classList.add("invalid")
+    }}
+}
+
+// FUNÇÕES PARA VALIDAR SE OS INPUTS NÃO ESTÃO VAZIOS E SE HÁ ALGUM INPUT INVÁLIDO (ou seja com o invalid na classe)
+
+// Abaixo, a função verifica se há algum input essencial vazio
+// Retorna True se os inputs estiverem preenchidos
+function verInputs(nomePagina) {
+  let pagina = document.querySelector(nomePagina);
+  let lista_inputs=pagina.querySelectorAll("input");
+  for (i=0;i<lista_inputs.length;i++){
+    let item = lista_inputs[i];
+
+    // A primeira condição é para PÁGINA_DOIS , verificar se a resposta incorreta 1 está preenchida, as outras não têm a mesma obrigatoriedade
+    
+    if(item.classList.contains("dois") || item.classList.contains("tres")) {
+      let primeiro = item.parentNode.children[0];
+      let segundo = item.parentNode.children[1];
+      if((primeiro.value!=="" && segundo.value==="") || (primeiro.value==="" && segundo.value!=="")){
+        alert("Algum campo não foi preenchido. Por favor, verifique.")
+        return false
+      } 
+    }else {
+      if(item.value===""){
+        alert("Algum campo não foi preenchido. Por favor, verifique.")
+        return false
+      }
+    }
+    if(i===lista_inputs.length-1){
+      return true
+    }
+  }
+ 
+}
+
+// Abaixo, a função verifica se há algum input tem classe invalid
+// Retorna True se os inputs não tiverem invalid
+function verInvalid(nomePagina) {
+  let pagina = document.querySelector(nomePagina)
+
+  // Verifica se há algum elemento com a classe invalid
+  if(pagina.querySelector(".invalid")!==null){
+    let elemPai = pagina.querySelector(".invalid").parentNode;
+    if(elemPai.classList[0]==="dupla_input" && nomePagina===".pagina_dois"){
+      alert("Verifique o item Respostas Incorretas, há algo inválido.")
+      return false
+    }else{
+      alert(`Verifique o item  ${elemPai.innerText}, há algo inválido.`)
+      return false
+    }
+  }else {
+    return true
+  }
+}
+
+
+// FIM DE VERIFICAÇÕES DOS INPUTS   ---------------------------------------
+
+
+
 function pagina_um() {
   let pagina_um = document.querySelector(".pagina_um");
   esconderElemento(".conteudo");
@@ -161,13 +290,13 @@ function pagina_um() {
 
     <div class="bloco_inputs" >
 
-        <input class="primeiro_input" minlength="20" maxlength="65" type="text" required placeholder="Titulo do seu Quizz"/>
+        <input class="primeiro_input inputs" minlength="20" maxlength="65" type="text" required placeholder="Titulo do seu Quizz"/>
 
-        <input class="segundo_input" type="url" required placeholder="URL da imagem do seu quizz"/>
+        <input class="segundo_input inputs" type="url" required placeholder="URL da imagem do seu quizz"/>
 
-        <input class="terceiro_input" type="text" placeholder="Quantidade de perguntas do quizz"/>
+        <input class="terceiro_input inputs" type="text" placeholder="Quantidade de perguntas do quizz"/>
 
-        <input class="quarto_input" type="text" placeholder="Quantidade de níveis do quizz"/>
+        <input class="quarto_input inputs" type="text" placeholder="Quantidade de níveis do quizz"/>
     </div>
   </div>
   <div class="button">
@@ -201,7 +330,18 @@ function especificacoesQuizz() {
 
 }
 
+
 // TELA 3.2 : PERGUNTAS DO QUIZ (conforme requisitos no notion)
+
+
+// Para validar e armazenar as perguntas 
+function addPerguntas(){
+  // Verifica se não inputs inválidos e/ou vazios
+  if(verInvalid(".pagina_dois") && verInputs(".pagina_dois")){
+    alert("sucesssooooooooooooo")
+  }
+}
+
 // Adiciona o formulário de perguntas da etapa dois da criação
 function pagina_dois() {
   let pagina_dois = document.querySelector(".pagina_dois");
@@ -216,27 +356,27 @@ function pagina_dois() {
     <div class="formulario" >
       <div class="bloco_inputs" >
           <div class="bold" >Pergunta 1</div>
-          <input class="primeiro_input" type="text" placeholder="Texto da Pergunta" />
-          <input class="segundo_input" type="text" placeholder="Cor de fundo da pergunta" />
+          <input class="inputs" onchange='numCaracter( this, 20, false)' type="text" placeholder="Texto da Pergunta" />
+          <input class="inputs" onchange='hexColor(this)' type="text" placeholder="Cor de fundo da pergunta (hexadecimal)" />
       </div>
       <div class="bloco_inputs" >
           <div class="bold" >Resposta Correta</div>
-          <input class="primeiro_input" type="text" placeholder="Resposta correta" />
-          <input class="segundo_input" type="text" placeholder="URL da imagem" />
+          <input class="inputs" onchange='numCaracter( this, 1, false)' type="text" placeholder="Resposta correta" />
+          <input class="inputs"  onchange='numCaracter( this, 1, false),verifURL(this)' type="text" placeholder="URL da imagem" />
       </div>
       <div class="bloco_inputs" >
           <div class="bold" >Respostas Incorretas</div>
           <div class="dupla_input">
-            <input class="primeiro_input" type="text" placeholder="Resposta Incorreta 1" />
-            <input class="segundo_input" type="text" placeholder="URL da imagem 1" />
+            <input class="inputs um" onchange='numCaracter( this, 1, false)'  type="text" placeholder="Resposta Incorreta 1" />
+            <input class="inputs um" onchange='verifURL(this)' type="text" placeholder="URL da imagem 1" />
           </div>
           <div class="dupla_input">
-            <input class="primeiro_input" type="text" placeholder="Resposta Incorreta 2" />
-            <input class="segundo_input" type="text" placeholder="URL da imagem 2" />
+            <input class="inputs dois" type="text" placeholder="Resposta Incorreta 2" />
+            <input class="inputs dois" onchange='verifURL(this)' type="text" placeholder="URL da imagem 2" />
           </div>
           <div class="dupla_input">
-            <input class="primeiro_input" type="text" placeholder="Resposta Incorreta 3" />
-            <input class="segundo_input" type="text" placeholder="URL da imagem 3" />
+            <input class="inputs tres" type="text" placeholder="Resposta Incorreta 3" />
+            <input class="inputs tres" onchange='verifURL(this)' type="text" placeholder="URL da imagem 3" />
           </div>
       </div>
     </div>
@@ -253,12 +393,12 @@ function pagina_dois() {
       </div>
     </div>
     <div class="button">
-          <button type="submit">Prosseguir para criar níveis</button>
+          <button onclick='addPerguntas()' type="submit">Prosseguir para criar níveis</button>
     </div>
   `
 }
-//pagina_dois()
 
+pagina_dois();
 
 // TELA 3.4: SUCESSO DO QUIZ (conforme requisitos no notion)
 // Adiciona uma mensagem de sucesso, representando a finalização da criação do quizz
@@ -283,11 +423,11 @@ function pagina_quatro () {
     </div>
     <div class="button">
           <button onclick='exibir(this)' class='${criacao_id}' >Acessar Quizz</button>
-          <div onclick='voltar_home()' class="titulo_cinza"> Voltar para home </div>
+          <div onclick='voltar_home()' class="texto_cinza"> Voltar para home </div>
     </div>
   ` 
 }
-pagina_quatro()
+//pagina_quatro()
 
 // FIM DA CRIAÇÃO  DE  UM QUIZZ    --------------------------
 
